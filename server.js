a4e65ceb52c7ef7cba1bb1259778ff82d1955a11
Iain Bromley <iaineubromley@gmail.com>
2015-12-26 20:52:24 -0800

    Added sync capabilities

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+/* server.js */
 
 var express = require('express');
 var app = express();
@@ -13,6 +13,21 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('pad');
 });
+app.get('/(:id)', function(req, res) {
+  res.render('pad');
+});
+
+// get sharejs dependencies
+var sharejs = require('share');
+require('redis');
+
+// options for sharejs
+var options = {
+  db: {type: 'redis'},
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
